cb5b6e1b4ba8f667bc36033f30171fa2b7279f8d
Baksling <57626255+Baksling@users.noreply.github.com>
2022-09-12 12:52:21 +0200

    Create run_server.ps1

---
+++ run_server.ps1
@@ -0,0 +1,22 @@
+
+
+$local_folder = "./src"
+$server_path = "/home/ubuntu/P7/"
+
+function Move-Files{
+    param([string] $ssh_target)
+
+    if([string]::IsNullOrWhiteSpace($ssh_target) -or $Name -like "* *"){
+        Write-Error "You didnt add a ssh target you idiot!"
+        return;
+    }
+
+    Write-Output("Starting transfering files...")
+    Invoke-Expression("scp -r " + $local_folder + " " + $ssh_target + ":" + $server_path )
+    Write-Output("Files transfered to '" + $ssh_target + "'.")
+}
+
+
+$ssh_target = $args[0]
+
+Move-Files($ssh_target)
