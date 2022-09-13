a9ca473189737e3af01b810a35b290c4e0ca9172
Baksling <57626255+Baksling@users.noreply.github.com>
2022-09-13 10:17:18 +0200

    compiles when uploading

--- run_server.ps1
+++ run_server.ps1
@@ -1,7 +1,14 @@
 
 
-$local_folder = "./src2"
-$server_path = "/home/ubuntu/P7/"
+#local_folder must not start with ./ or end with /
+$local_folder = "src2"
+
+#server_path must end with '/'
+$server_path = "/home/ubuntu/P7"
+
+#The output file from the compiler
+$compile_filename = "a.out"
+
 
 function Move-Files{
     param([string] $ssh_target)
@@ -12,11 +19,38 @@ function Move-Files{
     }
 
     Write-Output("Starting transfering files...")
-    Invoke-Expression("scp -r " + $local_folder + " " + $ssh_target + ":" + $server_path )
+    Invoke-Expression("scp " + $local_folder + "/* " + $ssh_target + ":" + $server_path )
     Write-Output("Files transfered to '" + $ssh_target + "'.")
+}
+
+function Compile_project{
+    param([string] $ssh_target)
+
+    if([string]::IsNullOrWhiteSpace($ssh_target) -or $Name -like "* *"){
+        Write-Error "You didnt add a ssh target you idiot!"
+        return;
+    }
+
+    $compile_command = "g++ " + $server_path + "/*.cpp -o " + $server_path + "/" + $compile_filename;
+
+    Write-Output("Start Compiling")
+    Invoke-Expression("ssh " + $ssh_target + " " + $compile_command)
+    Write-Output("Compilation concluded")
+}
+
+function Run_project(){
+    param([string] $ssh_target)
+
+    if([string]::IsNullOrWhiteSpace($ssh_target) -or $Name -like "* *"){
+        Write-Error "You didnt add a ssh target you idiot!"
+        return;
+    }
+
+    
 }
 
 
 $ssh_target = $args[0]
 
 Move-Files($ssh_target)
+Compile_project($ssh_target)
