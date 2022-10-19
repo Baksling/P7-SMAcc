ba564e7c8c12c529dbd4fb00bd615168ad1a03bd
Andemand42 <57626278+Andemand42@users.noreply.github.com>
2022-10-19 11:39:36 +0200

    Fixed compile.sh for real this time

--- src2/Cuda/compile.sh
+++ src2/Cuda/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # nvcc script
-nvcc main.cu ./Simulator/*.cu ./UPPAALTreeParser/*.cu ./UPPAALTreeParser/*.cpp ./Domain/*.cu ./Visitors/*.cu ./Domain/expressions/*.cu -o ./a.out -rdc=true
+nvcc main.cu ./Simulator/*.cu ./UPPAALTreeParser/*.cu ./UPPAALTreeParser/*.cpp ./Domain/*.cu ./Visitors/*.cu ./Domain/expressions/*.cu -rdc=true -o ./a.out
