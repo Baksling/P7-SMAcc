53d07b9985d036264119d6dd57afd506917ad18d
Andemand42 <57626278+Andemand42@users.noreply.github.com>
2022-11-25 10:03:54 +0100

    fixed compile scripts

---
+++ src2/Cuda/compile_old.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# nvcc script
+nvcc main.cu ./Simulator/*.cu ./Simulator/writers/*.cu ./UPPAALTreeParser/*.cu ./UPPAALTreeParser/*.cpp ./Domain/*.cu ./Visitors/*.cu ./Domain/expressions/*.cu -rdc=true --use_fast_math -O3 -o ./a.out
