c0fa5c7875987146c5aea33e39dc3e8f2c3f8f99
Baksling <57626255+Baksling@users.noreply.github.com>
2023-03-16 11:43:00 +0100

    Update abstract_parser.h

--- src2/Cuda/UPPAALXMLParser/abstract_parser.h
+++ src2/Cuda/UPPAALXMLParser/abstract_parser.h
@@ -16,6 +16,23 @@ public:
     virtual std::unordered_map<int, std::string>* get_clock_names() = 0;
     virtual std::unordered_map<int, int>* get_subsystems() = 0;
 
+    static unordered_set<std::string>* parse_query(const std::string& query)
+    {
+        if(query.find(' ') != std::string::npos)
+            throw std::runtime_error("Query contains space");
+        
+        //p0.Node0,P1.Node2,P1.Node0
+        const std::list<std::string> split = helper::split_all(query, ",");
+        unordered_set<std::string>* set = new unordered_set<std::string>();
+        for(auto& q : split)
+        {
+            if(std::count(q.begin(), q.end(), '.') != 1)
+                throw std::runtime_error("Query contains section with more or less than 1 dot ("+ q +")");
+            set->insert(q);
+        }
+        return set;
+    }
+    
     static bool try_parse_block_threads(const std::string& str, unsigned* out_blocks, unsigned* out_threads)
     {
         const std::list<std::string> split = helper::split_all(str, ",");
